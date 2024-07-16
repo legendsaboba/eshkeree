6f056023438524084041187c4b234ff23411f3b5
egor <nosok@tutamail.com>
2024-07-16 15:57:19 +0500

    v2.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-token = '7071800191:AAGIs28xt9bsQonvemOezuxjG0K1M9U9nGI'
+token = '7431236563:AAFtcP2OSZ1MGXHuc6VgugrU-Apuejq8Pzk'
 
 bot = telebot.TeleBot(token)
 
@@ -8,8 +8,7 @@ bot = telebot.TeleBot(token)
 def send_welcome(message):
     name = message.from_user.username
     print(name)
-    bot.reply_to(message,'привет',name)
-
+    bot.send_message(message,'привет')
 
 @bot.message_handler(fync=lambda message: True)
 def echo_message(message):
@@ -20,7 +19,26 @@ def echo_message(message):
     for symbol in text_message:
         bot.reply_to(message, symbol)
 
-t.me/+SMh9YT_AOnl4MjQ6
+@bot.message_handler(commands=['random1'])
+def message2(message):
+    chat_ID = message.from_user.id
+    bot_message = bot.send_dice(chat_ID, '⚽')
+    print(bot_message.dice.value)
+
+@bot.message_handler(commands=['sticker'])
+def sticker(message):
+    chat_ID = message.from_user.id
+    bot.send_sticker(chat_ID, '⚽')
+
+@bot.message_handler(commands=['doc'])
+def doc(message):
+    chat_ID = message.from_user.id
+    bot.send_document(chat_ID, open('dfdfsdfdfsf.txt', 'rb'))
+
+@bot.message_handler(commands=['photo'])
+def photo(message):
+    chat_ID = message.from_user.id
+    bot.send_photo(chat_ID, 'https://s0.rbk.ru/v6_top_pics/media/img/5/31/756806793338315.png')
 
 bot.infinity_polling()
 
